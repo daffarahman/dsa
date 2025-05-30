da24af192dcbecbc715fe8c110aa034eb42e174e
daffarahman <69158099+daffarahman@users.noreply.github.com>
2025-05-30 07:33:16 +0700

    refactor

--- praktikum/graph_disjoint/L0124062_MuhammadDaffaRahman_Graph_AdjacencyList.java
+++ praktikum/graph_disjoint/L0124062_MuhammadDaffaRahman_Graph_AdjacencyList.java
@@ -3,18 +3,18 @@ package praktikum.graph_disjoint;
 public class L0124062_MuhammadDaffaRahman_Graph_AdjacencyList {
     public static void main(String[] args) {
         AdjacencyListWeightedDirectedGraph someGraph = new AdjacencyListWeightedDirectedGraph(8);
-        someGraph.add(0, 1, 9); // A -> B (9)
-        someGraph.add(0, 2, 6); // A -> C (6)
-        someGraph.add(0, 7, 10); // A -> H (10)
-        someGraph.add(2, 1, 6); // C -> B (6)
-        someGraph.add(2, 3, 11); // C -> D (11)
-        someGraph.add(2, 4, 7); // C -> E (7)
-        someGraph.add(4, 1, 10); // E -> B (10)
-        someGraph.add(4, 3, 20); // E -> D (20)
-        someGraph.add(4, 5, 12); // E -> F (12)
-        someGraph.add(4, 6, 8); // E -> G (8)
-        someGraph.add(6, 5, 7); // G -> F (7)
-        someGraph.add(7, 3, 13); // H -> D (13)
+        someGraph.addEdge(0, 1, 9); // A -> B (9)
+        someGraph.addEdge(0, 2, 6); // A -> C (6)
+        someGraph.addEdge(0, 7, 10); // A -> H (10)
+        someGraph.addEdge(2, 1, 6); // C -> B (6)
+        someGraph.addEdge(2, 3, 11); // C -> D (11)
+        someGraph.addEdge(2, 4, 7); // C -> E (7)
+        someGraph.addEdge(4, 1, 10); // E -> B (10)
+        someGraph.addEdge(4, 3, 20); // E -> D (20)
+        someGraph.addEdge(4, 5, 12); // E -> F (12)
+        someGraph.addEdge(4, 6, 8); // E -> G (8)
+        someGraph.addEdge(6, 5, 7); // G -> F (7)
+        someGraph.addEdge(7, 3, 13); // H -> D (13)
         someGraph.printList();
     }
 }
@@ -28,7 +28,7 @@ class AdjacencyListWeightedDirectedGraph {
         this.maxVertices = maxVertices;
     }
 
-    public void add(int from, int to, int weight) {
+    public void addEdge(int from, int to, int weight) {
         if (from >= 0 && from < maxVertices) {
             Edge current = list[from];
             if (current == null) {
